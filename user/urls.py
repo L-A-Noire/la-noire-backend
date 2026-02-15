4036d613a29b567ee68684d5b33b2af9043a152c
melikaalizade <melikaalizadeh7@gmail.com>
2026-02-15 11:07:38 +0330

    feat(user): Add role management routes

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from user.views import RegisterView, LoginView, UserDetailView
+from user.views import RegisterView, LoginView, UserDetailView, RoleListCreateView, RoleRetrieveUpdateDestroyView
 
 urlpatterns = [
+    # Auth URLs
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', UserDetailView.as_view(), name='user-detail'),
+
+    # Role Management URLs (Admin only)
+    path('roles/', RoleListCreateView.as_view(), name='role-list-create'),
+    path('roles/<int:pk>/', RoleRetrieveUpdateDestroyView.as_view(),
+         name='role-detail'),
 ]
