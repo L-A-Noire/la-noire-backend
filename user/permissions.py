c9fcd6c8ae069b8ad18c0bcbeeff0c666d33a14b
melikaalizade <melikaalizadeh7@gmail.com>
2026-02-15 11:03:29 +0330

    feat(user): Add custom IsAdminUser permission for role management

---
+++ user/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+
+class IsAdminUser(permissions.BasePermission):
+    """
+    Custom permission to only allow Administrator users to access.
+    """
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.role and request.user.role.title == 'Administrator')
