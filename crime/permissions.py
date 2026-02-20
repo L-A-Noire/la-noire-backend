beb3326719142c98aa73cb1cf2f6083cc161c812
melikaalizade <melikaalizadeh7@gmail.com>
2026-02-21 02:38:29 +0330

    feat(crime): Add role-based permissions

---
+++ crime/permissions.py
@@ -0,0 +1,49 @@
+from rest_framework import permissions
+
+
+class IsCadet(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.role and request.user.role.title == 'Cadet')
+
+
+class IsSergeant(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.role and request.user.role.title == 'Sergeant')
+
+
+class IsCaptain(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.role and request.user.role.title == 'Captain')
+
+
+class IsChief(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.role and request.user.role.title == 'Chief')
+
+
+class IsPoliceOfficer(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.role and request.user.role.title == 'Police/Patrol Officer')
+
+
+class IsDetective(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.role and request.user.role.title == 'Detective')
+
+
+class IsComplainant(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.role and request.user.role.title == 'Complainant')
+
+
+class IsBaseUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.role and request.user.role.title == 'Base User')
