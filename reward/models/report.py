6cb14cf66fef3db35775f4bea1b31e3d10ef7338
melikaalizade <melikaalizadeh7@gmail.com>
2026-02-21 23:54:17 +0330

    feat(reward): Add Reward models

--- reward/models/report.py
+++ reward/models/report.py
@@ -1,5 +1,75 @@
 from django.db import models
+from crime.models import Case
+from suspect.models import SuspectCrime
 
 
 class Report(models.Model):
-    pass
+    reporter = models.ForeignKey(
+        to="user.User", on_delete=models.PROTECT, related_name="tips"
+    )
+
+    case = models.ForeignKey(
+        Case, on_delete=models.CASCADE, null=True, blank=True, related_name="tips"
+    )
+
+    suspect = models.ForeignKey(
+        SuspectCrime,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="tips",
+    )
+
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    STATUS_CHOICES = (
+        ("pending_officer", "Pending Officer"),
+        ("rejected_by_officer", "Rejected by Officer"),
+        ("pending_detective", "Pending Detective"),
+        ("rejected_by_detective", "Rejected by Detective"),
+        ("approved", "Approved"),
+    )
+
+    status = models.CharField(
+        max_length=30, choices=STATUS_CHOICES, default="pending_officer"
+    )
+
+    officer = models.ForeignKey(
+        to="user.User",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="reviewed_tips_officer",
+    )
+
+    officer_rejection_reason = models.TextField(blank=True, null=True)
+
+    detective = models.ForeignKey(
+        to="user.User",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="reviewed_tips_detective",
+    )
+
+    detective_rejection_reason = models.TextField(blank=True, null=True)
+
+    reward = models.OneToOneField(
+        to="Reward",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="report",
+    )
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="either_case_or_suspect",
+                condition=(
+                    models.Q(case__isnull=False, suspect__isnull=True)
+                    | models.Q(case__isnull=True, suspect__isnull=False)
+                ),
+            )
+        ]
--- reward/models/reward.py
+++ reward/models/reward.py
@@ -1,5 +1,20 @@
 from django.db import models
+import uuid
 
 
 class Reward(models.Model):
-    pass
+    unique_code = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+
+    recipient = models.ForeignKey(
+        to="user.User", on_delete=models.PROTECT, related_name="rewards"
+    )
+
+    amount = models.BigIntegerField(default=0)
+
+    is_claimed = models.BooleanField(default=False)
+    claimed_at = models.DateTimeField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        to="user.User", on_delete=models.PROTECT, related_name="issued_rewards"
+    )
