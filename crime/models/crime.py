9717bef7680b7eaea9cb9d3e0f8da8bacbcb5a1e
melikaalizade <melikaalizadeh7@gmail.com>
2026-02-21 05:18:23 +0330

    refactor(crime): Update critical severity value in Crime model

--- crime/models/crime.py
+++ crime/models/crime.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Crime(models.Model):
     LEVEL_CHOICES = (
-        ("0", "critical"),
         ("1", "level_1"),
         ("2", "level_2"),
         ("3", "level_3"),
+        ("4", "critical"),
     )
 
     title = models.CharField(max_length=200, null=True)
