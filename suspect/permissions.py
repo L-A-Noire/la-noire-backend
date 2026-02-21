b82a51794420094db0a52e758c165deb8e56f299
melikaalizade <melikaalizadeh7@gmail.com>
2026-02-21 06:56:37 +0330

    feat(suspect): Add role-based permissions for suspect

---
+++ suspect/permissions.py
@@ -0,0 +1,31 @@
+from rest_framework import permissions
+
+
+class IsDetective(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.role and request.user.role.title == 'Detective')
+
+
+class IsSergeant(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.role and request.user.role.title == 'Sergent')
+
+
+class IsCaptain(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.role and request.user.role.title == 'Captain')
+
+
+class IsJudge(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.role and request.user.role.title == 'Judge')
+
+
+class IsChief(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.role and request.user.role.title == 'Chief')
