e23f868372785a755e98ecf106559530ae75415c
Samin Akbari <saminakbari.e@gmail.com>
2026-02-24 20:35:43 +0330

    fix: Define model in model serializer

--- witness/serializers.py
+++ witness/serializers.py
@@ -6,12 +6,13 @@ from .models import (
     Image,
     OtherEvidence,
     Testimony,
-    VehicleEvidence,
+    VehicleEvidence, Evidence,
 )
 
 
 class EvidenceSerializer(serializers.ModelSerializer):
     class Meta:
+        model = Evidence
         fields = "__all__"
         read_only_fields = ("id",)
 
