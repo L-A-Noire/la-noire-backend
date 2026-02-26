ee9ebd97e8044a9bbf3cfb5099dec30315e5d468
Samin Akbari <saminakbari.e@gmail.com>
2026-02-26 15:50:18 +0330

    fix: Confirm testimony if the related case is set

--- witness/models/testimony.py
+++ witness/models/testimony.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 from witness.models.attachment import Attachment
 from witness.models.evidence import Evidence
@@ -12,3 +14,9 @@ class Testimony(Evidence):
     )
 
     is_confirmed = models.BooleanField(default=False)
+
+
+@receiver(pre_save, sender=Testimony)
+def handle_confirmation(sender, instance, **kwargs):
+    if not instance.is_confirmed and instance.case is not None:
+        instance.is_confirmed = True
