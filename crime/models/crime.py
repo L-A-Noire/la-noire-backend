526337eb5bfdae65f10a236e7a9ddf470ab96559
Samin Akbari <saminakbari.e@gmail.com>
2026-02-26 13:51:18 +0330

    refactor: Make level field integer

--- crime/models/crime.py
+++ crime/models/crime.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Crime(models.Model):
     LEVEL_CHOICES = (
-        ("1", "level_3"),
-        ("2", "level_2"),
-        ("3", "level_1"),
-        ("4", "critical"),
+        (1, "level_3"),
+        (2, "level_2"),
+        (3, "level_1"),
+        (4, "critical"),
     )
 
-    level = models.CharField(max_length=50, choices=LEVEL_CHOICES)
+    level = models.IntegerField(max_length=50, choices=LEVEL_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
