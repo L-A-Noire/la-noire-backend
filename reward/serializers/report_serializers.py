1f629faefab547a4862fa1584785dccbba26ef4c
Samin Akbari <saminakbari.e@gmail.com>
2026-02-27 01:58:57 +0330

    fix: Fix report serializer

--- reward/serializers/report_serializers.py
+++ reward/serializers/report_serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from reward.models import Report
+from suspect.serializers.suspect_serializers import SuspectSerializer
 from user.seiralizers import UserSerializer
 from crime.serializers import CaseSerializer
-from suspect.serializers import SuspectCrimeSerializer
 
 
 class ReportSerializer(serializers.ModelSerializer):
+    suspect = SuspectSerializer(read_only=True)
+
     class Meta:
         model = Report
         fields = "__all__"
@@ -40,7 +42,7 @@ class ReportDetailSerializer(serializers.ModelSerializer):
     officer_details = UserSerializer(source="officer", read_only=True)
     detective_details = UserSerializer(source="detective", read_only=True)
     case_details = CaseSerializer(source="case", read_only=True)
-    suspect_details = SuspectCrimeSerializer(source="suspect", read_only=True)
+    suspect_details = SuspectSerializer(source="suspect", read_only=True)
     status_display = serializers.CharField(source="get_status_display", read_only=True)
 
     class Meta:
