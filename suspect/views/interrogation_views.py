0183ea8a7c7cf36964edbe46c171357d7f397562
Samin Akbari <saminakbari.e@gmail.com>
2026-02-27 23:19:53 +0330

    feat: Add filters

--- suspect/views/interrogation_views.py
+++ suspect/views/interrogation_views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -15,6 +16,8 @@ from suspect.serializers import (
 class InterrogationViewSet(viewsets.ModelViewSet):
     queryset = Interrogation.objects.all()
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['case']
 
     def get_serializer_class(self):
         if self.action == "create":
